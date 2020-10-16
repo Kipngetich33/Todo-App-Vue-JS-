dfe8555762deeb8c2f628b987ac5eb6db0d52e72
Kipngetich <khalifngeno@gmail.com>
2020-10-16 10:03:12 +0300

    create simple page showing a list of todos

---
+++ src/components/Todo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class='ui centered card'>
+        <div class="content">
+
+        <div class="header">
+            {{ todo.title}}
+        </div>
+
+        <div class='meta'>
+            {{ todo.project }}
+        </div>
+
+        <div class="exta content">
+            <span class="right floated edit icon">
+            <i class="edit icon"></i>
+            </span>
+        </div>
+
+        <div class='ui bottom attached green basic button' v-show="todo.done">
+            Completed
+        </div>
+
+        <div class='ui bottom attached red basic button' v-show="!todo.done">
+            Completed
+        </div>
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        props: ['todo'],
+    };
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- JavaScript expressions in Vue are enclosed in double curly brackets. -->
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo  v-for="(todo,index) in todos" v-bind:todo="todo" v-bind:key="index"></todo>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+  import Todo from './Todo';
+
+  export default {
+    props: ['todos'],
+
+    components:{
+        Todo,
+    }
+  };
+</script>
+<style>
+</style>
